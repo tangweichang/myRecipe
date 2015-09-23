90d10b1f516901145816052d95db0a867ef055c6
Tjtang <tangweichangjlu@gmail.com>
2015-09-23 20:23:15 +0000

    create page chef id update in heroku

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -13,7 +13,7 @@ class RecipesController < ApplicationController
    
    def create
       @recipe = Recipe.new(recipe_params)
-      @recipe.chef = Chef.find(4)
+      @recipe.chef = Chef.find(1)
       
       if @recipe.save 
          flash[:success] = "Your recipe was created successfully!"
